fe159d2520a4944c0fe5eccd0e21d1fd0b681ff4
Gil Barbara <gilbarbara@gmail.com>
2016-10-17 22:08:53 -0200

    add ControlledRouter utils

---
+++ app/scripts/utils/ControlledRouter.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import BrowserHistory from 'react-history/BrowserHistory';
+import StaticRouter from 'react-router/StaticRouter';
+import { shouldComponentUpdate } from 'utils/helpers';
+
+import { setLocation } from 'actions';
+
+
+export default class ControlledRouter extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.element.isRequired,
+    dispatch: React.PropTypes.func.isRequired,
+    location: React.PropTypes.object.isRequired
+  };
+
+  shouldComponentUpdate = shouldComponentUpdate;
+
+  render() {
+    const { location: appLocation, children, dispatch } = this.props;
+
+    return (
+      <BrowserHistory>
+        {({ history, action, location }) => {
+          if (location.pathname !== appLocation.pathname) {
+            setImmediate(() => {
+              dispatch(setLocation(location));
+            });
+          }
+
+          return (
+            <StaticRouter
+              action={action}
+              basename={location.hostname === 'gilbarbara.github.io' ? '/react-redux-observables-boilerplate' : ''}
+              blockTransitions={history.block}
+              key={appLocation.pathname}
+              location={location}
+              onPush={history.push}
+              onReplace={history.replace}>
+              {children}
+            </StaticRouter>
+          );
+        }}
+      </BrowserHistory>
+    );
+  }
+}
