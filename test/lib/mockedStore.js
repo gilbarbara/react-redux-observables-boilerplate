711f224e7a4e3fb4f018865f934f80dbcbded8dd
Gil Barbara <gilbarbara@gmail.com>
2016-10-17 22:09:22 -0200

    update mockedStore and jsdom options

--- test/lib/mockedStore.js
+++ test/lib/mockedStore.js
@@ -1,7 +1,8 @@
 import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import rootEpic from 'epics';
 import { createEpicMiddleware } from 'redux-observable';
 
 export const epicMiddleware = createEpicMiddleware(rootEpic);
 
-export default configureMockStore([epicMiddleware]);
+export default configureMockStore([thunk, epicMiddleware]);
--- test/lib/setup.js
+++ test/lib/setup.js
@@ -1,1 +1,12 @@
 import 'vendor/rxjs';
+
+Object.defineProperty(window.location, 'href', {
+  writable: true,
+  value: 'http://localhost:3000/'
+});
+
+Object.defineProperty(window.location, 'pathname', {
+  writable: true,
+  value: '/'
+});
+
