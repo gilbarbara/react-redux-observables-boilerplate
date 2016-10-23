d7312a9399f58565db8b6f367754d408a7ec6fe8
Gil Barbara <gilbarbara@gmail.com>
2016-10-22 23:16:42 -0200

    add "fake" react-router-redux with ConnectedRouter

--- app/scripts/containers/App.jsx
+++ app/scripts/containers/App.jsx
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import Match from 'react-router/Match';
 import Miss from 'react-router/Miss';
 import { MatchWhenAuthorized, RedirectWhenAuthorized } from 'utils/router';
-import ControlledRouter from 'utils/ControlledRouter';
-
+import { ConnectedRouter } from 'utils/react-router-redux';
 
 import Home from 'containers/Home';
 import Private from 'containers/Private';
 import Login from 'containers/Login';
 import NotFound from 'containers/NotFound';
 
+import Loader from 'components/Loader';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import SystemNotifications from 'components/SystemNotifications';
@@ -24,32 +24,37 @@ export class App extends React.Component {
 
   render() {
     const { app, dispatch, user } = this.props;
+    let html = (<Loader />);
 
-    return (
-      <ControlledRouter dispatch={dispatch} location={app.location}>
-        <div key="app" className="app">
-          <Header dispatch={dispatch} user={user} />
-          <main className="app__main">
-            <Match exactly={true} pattern="/" component={Home} />
-            <RedirectWhenAuthorized
-              exactly={true}
-              pattern="/login"
-              component={Login}
-              isAuthenticated={user.isAuthenticated}
-            />
-            <MatchWhenAuthorized
-              exactly={true}
-              pattern="/private"
-              component={Private}
-              isAuthenticated={user.isAuthenticated}
-            />
-            <Miss component={NotFound} />
-          </main>
-          <Footer />
-          <SystemNotifications dispatch={dispatch} app={app} />
-        </div>
-      </ControlledRouter>
-    );
+    if (app.rehydrated) {
+      html = (
+        <ConnectedRouter>
+          <div key="app" className="app">
+            <Header dispatch={dispatch} user={user} />
+            <main className="app__main">
+              <Match exactly={true} pattern="/" component={Home} />
+              <RedirectWhenAuthorized
+                exactly={true}
+                pattern="/login"
+                component={Login}
+                isAuthenticated={user.isAuthenticated}
+              />
+              <MatchWhenAuthorized
+                exactly={true}
+                pattern="/private"
+                component={Private}
+                isAuthenticated={user.isAuthenticated}
+              />
+              <Miss component={NotFound} />
+            </main>
+            <Footer />
+            <SystemNotifications dispatch={dispatch} app={app} />
+          </div>
+        </ConnectedRouter>
+      );
+    }
+
+    return html;
   }
 }
 
--- app/scripts/store/configureStore.dev.js
+++ app/scripts/store/configureStore.dev.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { createEpicMiddleware } from 'redux-observable';
+import { routerReducer } from 'utils/react-router-redux';
 
 import createLogger from 'redux-logger';
 import Reactotron from 'reactotron-react-js';
@@ -13,7 +14,7 @@ import { ActionTypes } from 'constants/index';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-const reducer = combineReducers({ ...rootReducer });
+const reducer = combineReducers({ ...rootReducer, router: routerReducer });
 
 const logger = createLogger({
   // predicate: (getState, action) => (action.type.indexOf('_REQUEST') === -1),
