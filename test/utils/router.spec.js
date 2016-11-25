a34d04998fb5565fbb163068e2471c5c1d27008e
Gil Barbara <gilbarbara@gmail.com>
2016-11-25 00:22:52 -0200

    kinda fix router tests. needs work

--- test/utils/router.spec.js
+++ test/utils/router.spec.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import Router from 'react-router/MemoryRouter';
 import { renderToString } from 'react-dom/server';
 import { MatchWhenAuthorized, RedirectWhenAuthorized } from 'utils/router';
 
 describe('utils/MatchWhenAuthorized', () => {
-  it('should prevent navigation for unauthenticated access', () => {
-    const render = renderToString(<MatchWhenAuthorized
-      exactly={true}
-      pattern="/private"
-      location={{ pathname: '/private' }}
-      component={() => (<div className="private" />)}
-      isAuthenticated={false}
-    />);
+  it('should redirect for unauthenticated access', () => {
+    const render = renderToString(
+      <Router intialEntries={[{ pathname: '/private' }]}>
+        <MatchWhenAuthorized
+          exactly={true}
+          pattern="/private"
+          component={() => (<div>PRIVATE</div>)}
+          isAuthenticated={false}
+        />
+      </Router>);
 
     expect(render).toMatchSnapshot();
   });
 
   it('should allow navigation for authenticated access', () => {
-    const render = renderToString(<MatchWhenAuthorized
-      exactly={true}
-      pattern="/private"
-      location={{ pathname: '/private' }}
-      component={() => (<div className="private" />)}
-      isAuthenticated={true}
-    />);
+    const render = renderToString(
+      <Router intialEntries={[{ pathname: 'private' }]}>
+        <MatchWhenAuthorized
+          exactly={true}
+          pattern="/private"
+          component={() => (<div>PRIVATE</div>)}
+          isAuthenticated={true}
+        />
+      </Router>
+    );
 
     expect(render).toMatchSnapshot();
   });
@@ -30,25 +36,31 @@ describe('utils/MatchWhenAuthorized', () => {
 
 describe('utils/RedirectWhenAuthorized', () => {
   it('should render the Login component for unauthenticated access', () => {
-    const render = renderToString(<RedirectWhenAuthorized
-      exactly={true}
-      pattern="/login"
-      location={{ pathname: '/login' }}
-      component={() => (<div className="login" />)}
-      isAuthenticated={false}
-    />);
+    const render = renderToString(
+      <Router intialEntries={['/']}>
+        <RedirectWhenAuthorized
+          exactly={true}
+          pattern="/login"
+          component={() => (<div>LOGIN</div>)}
+          isAuthenticated={false}
+        />
+      </Router>
+    );
 
     expect(render).toMatchSnapshot();
   });
 
   it('should redirect to /private for authenticated access', () => {
-    const render = renderToString(<RedirectWhenAuthorized
-      exactly={true}
-      pattern="/login"
-      location={{ pathname: '/login' }}
-      component={() => (<div className="login" />)}
-      isAuthenticated={true}
-    />);
+    const render = renderToString(
+      <Router intialEntries={['/']}>
+        <RedirectWhenAuthorized
+          exactly={true}
+          pattern="/login"
+          component={() => (<div>LOGIN</div>)}
+          isAuthenticated={true}
+        />
+      </Router>
+    );
 
     expect(render).toMatchSnapshot();
   });
