be1ffb4d1b9e7ed4ce373c06940728c813a70b62
Gil Barbara <gilbarbara@gmail.com>
2017-05-30 22:26:38 -0300

    upgrade Jest

--- test/__setup__/index.js
+++ test/__setup__/index.js
@@ -15,6 +15,27 @@ react.id = 'react';
 react.style.height = '100vh';
 document.body.appendChild(react);
 
+const consoleError = console.error;
+console.error = jest.fn(message => {
+  const skipMessages = [
+    'Accessing PropTypes via the main React package is deprecated. Use the prop-types package from npm instead.',
+    'React.createClass is deprecated',
+    'Shallow renderer has been moved',
+    'ReactTestUtils has been moved to react-dom/test-utils',
+  ];
+  let shouldSkip = false;
+
+  for (const s of skipMessages) {
+    if (message.includes(s)) {
+      shouldSkip = true;
+    }
+  }
+
+  if (!shouldSkip) {
+    consoleError(message);
+  }
+});
+
 window.matchMedia = () =>
   ({
     matches: false,
