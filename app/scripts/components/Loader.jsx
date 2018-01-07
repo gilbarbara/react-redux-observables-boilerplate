cb6a2f01f9b5d901f94590adad6917e74ec418d2
Gil Barbara <gilbarbara@gmail.com>
2018-01-07 01:37:11 -0200

    update dependencies

--- app/scripts/components/Loader.jsx
+++ app/scripts/components/Loader.jsx
@@ -2,37 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Loader = ({ pulse }) => {
-  let html;
-
   if (pulse) {
-    html = (
+    return (
       <div className="app__loader app__loader--pulse">
         <div />
       </div>
     );
   }
-  else {
-    html = (
-      <div className="app__loader app__loader--rotate">
-        <svg className="loader__svg">
-          <circle
-            className="loader__circle"
-            cx="50"
-            cy="50"
-            r="20"
-            fill="none"
-            strokeWidth="2"
-          />
-        </svg>
-      </div>
-    );
-  }
 
-  return html;
+  return (
+    <div className="app__loader app__loader--rotate">
+      <svg className="loader__svg">
+        <circle
+          className="loader__circle"
+          cx="50"
+          cy="50"
+          r="20"
+          fill="none"
+          strokeWidth="2"
+        />
+      </svg>
+    </div>
+  );
 };
 
 Loader.propTypes = {
-  pulse: PropTypes.bool.isRequired,
+  pulse: PropTypes.bool,
 };
 
 Loader.defaultProps = {
